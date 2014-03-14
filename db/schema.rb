e9c3747dee2179d14636235806cbb5f5e3cd65c6
Yessergire Mohamed <abmo@cs.helsinki.fi>
2014-03-14 20:44:01 +0200

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140313194315) do
+ActiveRecord::Schema.define(version: 20140314180713) do
 
   create_table "authors", force: true do |t|
     t.integer  "born"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20140313194315) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "book_authors", force: true do |t|
+    t.integer  "book_id"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "books", force: true do |t|
